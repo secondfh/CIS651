122618c0e742387408e89329d616db864f723b9f
secondfh <75648084+secondfh@users.noreply.github.com>
2020-12-07 19:05:50 -0500

    Add files via upload

---
+++ PupPatrol/app/src/main/java/com/example/puppatrol/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.puppatrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
